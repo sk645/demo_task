80d4e2779e148aaf8ecd2fb0704b62ac0b260ef0
V-Sunil Kumar <v-sunil.kumar@magicsw.com>
2020-09-27 12:51:38 +0530

    filter issue fixed

--- index.js
+++ index.js
@@ -1,75 +1,115 @@
+var queryResult = '';
+var launchYear = '';
+var successfulLaunch = '';
+var successfulLand = '';
 
-// document.getElementsByTagName('filterinnerWrapId').childNodes.on('hover', function(){
-//     document.getElementById('filterinnerWrapId').childNodes.addClass("active")
-// })
-var year = '';
-var launch = '';
-var land = '';
+var selectedYear = '';
+var selectedSuccessfulLaunch = '';
+var selectedSuccessfulLand = '';
+
+function onYearClick(e) {
+    if(!isNaN(e.target.id)){
+        selectedYear = e.target.id;
+    } 
+    spaceXUrl(selectedYear, selectedSuccessfulLaunch, selectedSuccessfulLand);
+}
+
+function onLaunchClick(e) {
+    if(e.target.id) {
+        if(e.target.id === 'launch-true') {
+            selectedSuccessfulLaunch = 'true';
+        } else if (e.target.id === 'launch-false') {
+            selectedSuccessfulLaunch = 'false';
+        }
+        spaceXUrl(selectedYear, selectedSuccessfulLaunch, selectedSuccessfulLand);
+    }
+}
+
+function onLandClick(e) {
+    if (e.target.id) {
+        if (e.target.id === 'land-true') {
+            selectedSuccessfulLand = 'true';
+        } else if (e.target.id === 'land-false') {
+            selectedSuccessfulLand = 'false';
+        }
+        spaceXUrl(selectedYear, selectedSuccessfulLaunch, selectedSuccessfulLand);
+    }
+}
+
 function spaceXUrl(year, launch, land) {
-
+    if(year!='') {
+        launchYear = year;
+    }
+    if (launch != successfulLaunch) {
+        successfulLaunch = launch;
+    }
+    if (land != successfulLand) {
+        successfulLand = land;
+    }
     let url = 'https://api.spacexdata.com/v3/launches?limit=100'
-    if (year !== '') {
-        url = url + `&launch_year=${year}`;
+    if (launchYear !== '') {
+        url = url + `&launch_year=${launchYear}`;
         var allClasses = document.getElementsByClassName('year-button');
         for (let i = 0; i < allClasses.length; i++) {
             allClasses[i].classList.remove("active");
         }
-        document.getElementById(year).classList.add('active')
+        document.getElementById(launchYear).classList.add('active')
     }
-    if (launch !== '') {
-        url = url + `&launch_success=${launch}`;
+    if (successfulLaunch !== '') {
+        url = url + `&launch_success=${successfulLaunch}`;
         var allClasses = document.getElementsByClassName('launch-button');
         for (let i = 0; i < allClasses.length; i++) {
             allClasses[i].classList.remove("active");
         }
-        document.getElementById('launch-'+launch).classList.add('active')
+        document.getElementById('launch-' + successfulLaunch).classList.add('active')
     }
-    if (land !== '') {
-        url = url + `&land_success==${land}`;
+    if (successfulLand !== '') {
+        url = url + `&land_success=${successfulLand}`;
         var allClasses = document.getElementsByClassName('land-button');
         for (let i = 0; i < allClasses.length; i++) {
             allClasses[i].classList.remove("active");
         }
-        document.getElementById('land-'+land).classList.add('active')
+        document.getElementById('land-' + successfulLand).classList.add('active')
     }
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            dataItem = data;
+            queryResult = data;
+            var myUl = document.getElementById('programListUl');
             var myLi = '';
-            for (i = 0; i < dataItem.length; i++) {
-                var myUl = document.getElementById('programListUl');
+            if(queryResult.length === 0 ) {
+                myLi = '<li>NO DATA FOUND</li>';
+            }
+            for (i = 0; i < queryResult.length; i++) {
+                const {links, mission_name, mission_id, launch_year, launch_success,rocket} = queryResult[i];
+                var mi = mission_id && mission_id.length ? mission_id.join(', ') : 'NA';
                 myLi += '<li>' +
-                    '<div class="imageBox"><img src=' + dataItem[i].links.mission_patch_small + ' alt="aircraftImage" /></div>' +
+                    '<div class="imageBox"><img src=' + links.mission_patch_small + ' alt="aircraftImage" /></div>' +
                     '<div class="boxContent">' +
-                    '<h5>' + dataItem[i].mission_name + '</h5>' +
+                    '<h5>' + mission_name + '</h5>' +
                     '<div class="missionIds">' +
                     '<strong>Mission Ids:</strong>' +
                     '<ul>' +
-                    '<li>' + dataItem[i].mission_id + '</li>' +
+                    '<li>' + mi + '</li>' +
                     '</ul>' +
                     '</div>' +
                     '<div class="listStyle clearfix">' +
                     '<strong>Launch Year:</strong>' +
-                    '<span>' + dataItem[i].launch_year + '</span>' +
+                    '<span>' + launch_year + '</span>' +
                     '</div>' +
                     '<div class="listStyle clearfix">' +
                     '<strong>Successful Launch: </strong>' +
-                    '<span>' + dataItem[i].launch_success + '</span>' +
+                    '<span>' + launch_success + '</span>' +
                     '</div>' +
                     '<div class="listStyle clearfix">' +
                     '<strong>Successful Landing:</strong>' +
-                    '<span>' + dataItem[i].launch_success + '</span>' +
+                    '<span>' + rocket.first_stage.cores[0].land_success+ '</span>' +
                     '</div>' +
                     '</div>' +
                     '</li>';
-                myUl.innerHTML = myLi;
-
-            }
-        })
+                }
+                myUl.innerHTML = myLi;    
+        }, error => {console.log(error)})
 }
 
 spaceXUrl('', '', '');
-
-
-
